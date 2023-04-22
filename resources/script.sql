48b179f22712cb3fb9f7d612b84ba03c0a4efbc7
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-04-21 22:58:34 -0300

    Fixes bug

--- resources/script.sql
+++ resources/script.sql
@@ -4,6 +4,14 @@ CREATE TABLE author (
     PRIMARY KEY (id)
 );
 
+CREATE TABLE product (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    price DOUBLE NOT NULL,
+    quantity INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE review (
     id INT NOT NULL AUTO_INCREMENT,
     stars INT NOT NULL,
@@ -13,14 +21,6 @@ CREATE TABLE review (
     PRIMARY KEY (id),
     FOREIGN KEY (reviewer_id) REFERENCES author(id),
     FOREIGN KEY (product_id) REFERENCES product(id)
-);
-
-CREATE TABLE product (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    price DOUBLE NOT NULL,
-    quantity INT NOT NULL,
-    PRIMARY KEY (id)
 );
 
 CREATE TABLE book (
